363e645bfd34bce5810fc973f44cba9c0fe00bea
kmg0485 <kmg0485@naver.com>
2022-09-28 15:57:57 +0900

    MTV 파악하기, DB 연동하기기.

--- content/views.py
+++ content/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from content.models import Feed
 
-# Create your views here.
+
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all().order_by('-id')
+        return render(request, 'Insta_Clone/main.html', context=dict(feed_list=feed_list))
